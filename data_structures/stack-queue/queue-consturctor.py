52347719ecd39f9586d303b5fb0819a0d7c52230
luka-danger <lukathevegan@gmail.com>
2024-01-16 13:14:19 -0700

    Create dequeue method

--- data_structures/stack-queue/queue-consturctor.py
+++ data_structures/stack-queue/queue-consturctor.py
@@ -32,10 +32,28 @@ class Queue:
             self.last = new_node
         # Increment length of list by 1 
         self.length += 1
-            
+
+    def dequeue(self):
+        if self.length == 0:
+            return None 
+        temp = self.first
+        # Reset pointers to None 
+        if self.length == 1:
+            self.first = None
+            self.last = None 
+        else: 
+            # Move pointer to next node 
+            self.first = self.first.next 
+            # Remove node from list 
+            temp.next = None 
+        self.length -= 1
+        return temp 
 
 my_queue = Queue(4)
 
 my_queue.enqueue(5)
+
+# Test dequeue 
+# my_queue.dequeue()
 
 my_queue.print_queue()
