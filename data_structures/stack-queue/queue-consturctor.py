1c4c5a637aac69cd771e5c725da5e9431e18f81a
luka-danger <lukathevegan@gmail.com>
2024-01-16 13:09:06 -0700

    Create enqueue method

--- data_structures/stack-queue/queue-consturctor.py
+++ data_structures/stack-queue/queue-consturctor.py
@@ -15,7 +15,27 @@ class Queue:
         while temp: 
             print(temp.value)
             temp = temp.next
+    
+    # Add itme to end of queue (Getting in line) 
+    def enqueue(self, value):
+        # Create new node
+        new_node = Node(value)
+        # First and last item in line 
+        if self.length == 0:
+            self.first == new_node
+            self.last == new_node
+        else: 
+            # Enqueue from end 
+            # Add new node to end 
+            self.last.next = new_node
+            # Move last pointer to new node
+            self.last = new_node
+        # Increment length of list by 1 
+        self.length += 1
+            
 
 my_queue = Queue(4)
+
+my_queue.enqueue(5)
 
 my_queue.print_queue()
