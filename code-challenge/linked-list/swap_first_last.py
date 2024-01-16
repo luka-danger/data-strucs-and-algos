f4f194c93fd184ff23f90f228df2adc5500297b4
luka-danger <lukathevegan@gmail.com>
2024-01-16 09:02:20 -0700

    Add solution

--- code-challenge/linked-list/swap_first_last.py
+++ code-challenge/linked-list/swap_first_last.py
@@ -1,23 +1,24 @@
 class Node:
-    def __init__(self, value): 
+    def __init__(self, value):
         self.value = value
         self.next = None
         self.prev = None
+        
 
-class DoublyLinkedList: 
-    def __init__(self, value): 
+class DoublyLinkedList:
+    def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-        self.length += 1
+        self.length = 1
 
     def print_list(self):
         temp = self.head
-        while temp:
+        while temp is not None:
             print(temp.value)
             temp = temp.next
-
-    def append(self,value):
+        
+    def append(self, value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -27,14 +28,26 @@ class DoublyLinkedList:
             new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
-        return True 
     
     def swap_first_last(self):
-        if self.length == 0:
+        if self.length is None or self.head == self.tail:
             return None
-        before = self.head
-        after = self.tail
-        self.tail = before
-        self.head = after
-        
+        self.head.value, self.tail.value = self.tail.value, self.head.value
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+
+
+print('DLL before swap_first_last():')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.swap_first_last()
+
+
+print('\nDLL after swap_first_last():')
+my_doubly_linked_list.print_list()
+
 
