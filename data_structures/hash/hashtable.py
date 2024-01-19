29cb3609b9241d1cf5f55653d68319ab5cf5dcd6
luka-danger <lukathevegan@gmail.com>
2024-01-18 19:34:01 -0700

    Create set items method
    Add items to hash table. Pretty cool

--- data_structures/hash/hashtable.py
+++ data_structures/hash/hashtable.py
@@ -12,11 +12,28 @@ class HashTable:
             # Multiply by any prime number
             # Use Modulo to get remainder (will be between 0 - 6: address space)
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
-            return my_hash
+        return my_hash
         
     def print_table(self):
         for i, val in enumerate(self.data_map):
+            # Output Hash Table 
+            # 4 : 'bolts', 1400 
             print(i, ": ", val)
-    
+
+    # Use hash method on key to create the address
+    def set_items(self, key, value):
+        # Computer address
+        index = self.__hash(key)
+        # Initialize empty list at address
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        # Put key value Pair into empty address
+        self.data_map[index].append([key, value])
+
 my_hash_table = HashTable()
+
+my_hash_table.set_items('bolts', 1400)
+my_hash_table.set_items('washers', 50)
+my_hash_table.set_items('lumber', 70)
+
 my_hash_table.print_table()
