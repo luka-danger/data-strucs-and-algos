edee7936de5e8c8215338ec83d8fd25d0fe4a717
luka-danger <lukathevegan@gmail.com>
2024-02-05 12:23:21 -0700

    Create Solution

--- code-challenge/arrays-hashing/contains-duplicate.py
+++ code-challenge/arrays-hashing/contains-duplicate.py
@@ -2,20 +2,20 @@
 # Given an integer array, nums, return true if any value appears at least twice in array
 # Return false if every element is distinct 
 
-# Pseudocode: 
-# For Loop (x in nums)
-# Iterate through each value in nums
-# If i = some value in nums, return false
-# If i != some value in nums, return true 
-
-#ideas: 
-# Put each number into new array and run a nested for loop to compare? 
-# O(n^2) - not best approach 
-
-# Sorted array could be O(logn) which would be more ideal 
-
 # Solution 
-nums = []
-for i in nums: 
-    x = i 
-        
+def containsDuplicate(nums):
+    hashset = set()
+    
+    for i in nums:
+        if i in hashset: 
+            return True
+        hashset.add(i)
+    return False
+
+# Return False
+myArray = [1, 2, 3, 4]
+print(containsDuplicate(myArray))
+
+# Return True
+myOtherArray = [5, 6, 7, 5]
+print(containsDuplicate(myOtherArray)) 
