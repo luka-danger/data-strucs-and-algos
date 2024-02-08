a13c1a054d2d50b81fdc550c5b6ec483633fd766
luka-danger <lukathevegan@gmail.com>
2024-02-07 20:44:49 -0700

    Iterate through length of string s
    Create for loop, increment by 1 each time character for every occurence in the string, add comments

--- code-challenge/arrays-hashing/valid-anagram.py
+++ code-challenge/arrays-hashing/valid-anagram.py
@@ -1,4 +1,13 @@
 def validAnagram(s, t,):
     if len(s) != len(t):
         return False
+    
+    # Initialize hashmap to store count of each character in s and tß
     countS, countT = {}, {}
+    # Iterate through length of string s 
+    for i in range(len(s)):
+        # Count the occurence of each character in string s 
+        # Increment the count of character by 1 for every instance
+        # If key does not exist in hashmap, use default value 0
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        
