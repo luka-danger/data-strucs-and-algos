7f6f316d6b9776b193c7c9ef8e0756883da9fdba
luka-danger <lukathevegan@gmail.com>
2024-02-07 21:09:49 -0700

    Idk

--- code-challenge/arrays-hashing/valid-anagram.py
+++ code-challenge/arrays-hashing/valid-anagram.py
@@ -2,12 +2,21 @@ def validAnagram(s, t,):
     if len(s) != len(t):
         return False
     
-    # Initialize hashmap to store count of each character in s and tß
+    # Initialize hashmaps to store count of each character in s and t
     countS, countT = {}, {}
-    # Iterate through length of string s 
+    # Iterate through length of string s and add characters to hashmaps
     for i in range(len(s)):
         # Count the occurence of each character in string s 
         # Increment the count of character by 1 for every instance
         # If key does not exist in hashmap, use default value 0
         countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    # Iterate through hashmaps to make sure they contain same characters
+    for c in countS:
+        if countS[c] != countT.get(c, 0):
+            return False
+    return True 
+
+# Run Time: O(s + t) --> O(n + n)
+
         
