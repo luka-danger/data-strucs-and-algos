b03d5b16a39e5d028fe8e8b46845f9fc3a0fc5fe
luka-danger <lukathevegan@gmail.com>
2024-10-29 17:53:53 -0600

    Add sliding window algo

---
+++ algorithms/sliding.py
@@ -0,0 +1,10 @@
+
+def sum_is_twenty(array, sub):
+   index = []
+   for i in range(len(array)):
+      if sum(array[i: i + sub]) == 20:
+         index.append(i)
+   return len(index)
+
+my_array = [7, 12, 8, 11, 9]
+print(sum_is_twenty(my_array, 2))
